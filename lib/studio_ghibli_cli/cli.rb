52a369e16cd9215cd6a0dec6b7c54d24ae76026e
Akili Humphrey <akilihumphrey@gmail.com>
2018-12-11 21:18:30 +0000

    modified cli

--- lib/studio_ghibli_cli/cli.rb
+++ lib/studio_ghibli_cli/cli.rb
@@ -3,13 +3,22 @@
 class StudioGhibliCli::CLI
   
   def call
-    puts "Studio Ghibli Movie Generator - Select a Film Year to Start"
-    list_film_years
+    puts "Welcome to the Studio Ghibli Movie Generator"
+    enter_y_to_start
+    film_year_list
+    
   end
   
-  def list_film_years
-    year_array = [1984, 1986, 1988, 1989, 1991, 1992, 1993, 1994, 1995, 1997, 1999, 2000, 2001, 2002, 2004, 2006, 2008, 
-		2009, 2010, 2011, 2013, 2014, 2018]
-	end
+  def enter_y_to_start
+    puts "Enter Y to start"
+  end
   
+  def film_year_list
+    puts <<-DOC
+    Enter a film year from the list to generate a Studio Ghibli film:
+      1984, 1986, 1988, 1989, 1991, 1992, 1993, 1994,
+      1995, 1997, 1999, 2000, 2001, 2002, 2004, 2006,
+      2008, 2009, 2010, 2011, 2013, 2014, 2018
+    DOC
+  end
 end
