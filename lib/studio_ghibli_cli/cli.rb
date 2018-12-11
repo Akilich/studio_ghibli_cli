f5a52adb79949e5266cf6f29eac72767b671bd22
Akili Humphrey <akilihumphrey@gmail.com>
2018-12-11 22:07:25 +0000

    modified cli

--- lib/studio_ghibli_cli/cli.rb
+++ lib/studio_ghibli_cli/cli.rb
@@ -3,15 +3,34 @@
 class StudioGhibliCli::CLI
   
   def call
-    welcome_message
-    film_year_list
+    input = ''
+    while input != 'exit'
+    
+    puts "Welcome to the Studio Ghibli Movie Generator!"
+    puts "Generate a random Studio Ghibli film by its producer or Rotten Tomatoes score."
+    puts "To list all film producers, enter 'list producers'."
+    puts "To list all Rotten Tomatoes scores, enter 'list scores'."
+    puts "To generate a random film by producer, enter the name of the producer."
+    puts "To generate a random film by Rotten Tomatoes score, enter the score."
+    puts "To quit, type 'exit'."
+    puts "Enter a list to get started."
+    input = gets.chomp
+      case input
+      when 'list producers'
+        list_producers
+        when 'list scores'
+        list_scores
+      when 'producer name'
+        generate_from_producer
+      when 'score'
+        generate_from_score
+      end
+    end
   end
   
-  def welcome_message
-    puts "Welcome to the Studio Ghibli Movie Generator"
-  end
   
-  def film_year_list
+  
+  def list_producers
     puts <<-DOC
     Enter a film year from the list to generate a Studio Ghibli film:
       1984, 1986, 1988, 1989, 1991, 1992, 1993, 1994,
@@ -19,4 +38,15 @@ class StudioGhibliCli::CLI
       2008, 2009, 2010, 2011, 2013, 2014, 2018
     DOC
   end
+  
+  def list_scores
+  end
+
+  def generate_from_producer
+  end
+
+  def generate_from_score
+  end
+
 end
+
